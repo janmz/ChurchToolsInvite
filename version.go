4f702964c32436407121522bf4f6aab7b3960ca1
janmz <132650909+janmz@users.noreply.github.com>
2026-06-16 21:48:24 +0200

    Fix: Really upload executables

--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package main
 
 var (
-	Version   = "2.1.0.10" // Major, Minor, Patch, Build
-	BuildTime = "2026-06-16 21:46:08"
+	Version   = "2.1.1.11" // Major, Minor, Patch, Build
+	BuildTime = "2026-06-16 21:48:24"
 )
